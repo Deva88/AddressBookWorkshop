9875501566cd1e6eed8cbebfd2dc45db9d6fa7d5
Deva88 <devendra.raj.sdm@gmail.com>
2021-05-06 09:02:53 +0530

    Added:  Create a contacts Address Book

--- main/java/com/addressbook/AddressBook.java
+++ main/java/com/addressbook/AddressBook.java
@@ -1,8 +1,13 @@
 package com.addressbook;
+/*@Description- this class represents a address book.
+ *contact information  */
+/*@Parameters- taken Contacts class object as variable so that we can
+ *  add a new contact to AddressBook*/
 public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book");
+
 
     }
 
